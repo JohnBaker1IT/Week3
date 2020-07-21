c763f0c1696c3e0362a9da86349536c017e9ed2a
andybakerman <67850347+andybakerman@users.noreply.github.com>
2020-07-21 16:26:31 -0500

    Array List added

---
+++ Week 3/src/com/to/Collections/C1ArrayList.java
@@ -0,0 +1,20 @@
+package com.to.Collections;
+import java.util.ArrayList; 
+import java.util.Iterator;
+
+public class C1ArrayList {
+	public static void main(String... args) {
+		ArrayList<String> a1 = new ArrayList<String>(); 
+		a1.add("java");
+		a1.add("spring"); 
+		a1.add("java");
+		a1.set(1, "hybris");
+		Iterator<String> itr = a1.iterator();
+		
+		while(itr.hasNext()) {
+		System.out.println(itr.next());
+		}
+		a1.remove(1);
+		System.out.println(a1.get(1));
+	}
+}
