7950f2a2c546bcc1030408ba0af70f80d48766bd
andybakerman <67850347+andybakerman@users.noreply.github.com>
2020-07-23 13:10:59 -0500

    using iterator

--- Week 3/src/com/to/Collections/P1.java
+++ Week 3/src/com/to/Collections/P1.java
@@ -1,7 +1,11 @@
 package com.to.Collections;
 
 import java.util.HashMap;
+import java.util.Set;
 import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Iterator;
+import java.util.Map;
 
 /*
  * A. P-1 1. Should be able to add the person details (Name, Age, Gender,
@@ -27,19 +31,43 @@ public class P1 {
         
         System.out.println(infoList);
         
+     // Creating object of ListIterator<String> 
+        // using listIterator() method 
+        ListIterator<String> 
+            iterator = infoList.listIterator(); 
+
+        // Printing the iterated value 
+        System.out.println("\nUsing ListIterator:\n"); 
+        while (iterator.hasNext()) { 
+            System.out.println("Value is : "
+                               + iterator.next()); 
+        } 
+        
         HashMap<String, Integer> phonebook = new HashMap<>();
         phonebook.put("Kevin", 12345);
         phonebook.put("Jill", 98765);
         phonebook.put("Brenda", 123123);
         phonebook.put("Brenda", 22222);
         phonebook.put(null, 000);
-		/*
-		 * if(phonebook.containsKey("Brenda")) { phonebook.remove("Brenda"); }
-		 */
-        //phonebook.clear();
+		
         System.out.println(phonebook);
+        
+        
+     // Getting a Set of Key-value pairs
+        Set entrySet = phonebook.entrySet();
+     
+        // Obtaining an iterator for the entry set
+        Iterator it = entrySet.iterator();
+     
+        // Iterate through HashMap entries(Key-Value pairs)
+        System.out.println("HashMap Key-Value Pairs : ");
+        while(it.hasNext()){
+           Map.Entry me = (Map.Entry)it.next();
+           System.out.println("Key is: "+me.getKey() + 
+           " & " + 
+           " value is: "+me.getValue());
         
     }
 
-    
+   }
 }
